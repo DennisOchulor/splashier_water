10d878a40d5bbf09df8ed2cef73391bdccc5d2d0
Dennis Ochulor <dennisnwanevu@gmail.com>
2026-04-05 09:33:34 +0800

    @NullMarked all packages

---
+++ src/client/java/io/github/dennisochulor/splashier_water/client/package-info.java
@@ -0,0 +1,5 @@
+
+@NullMarked
+package io.github.dennisochulor.splashier_water.client;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/io/github/dennisochulor/splashier_water/mixin/package-info.java
@@ -0,0 +1,5 @@
+
+@NullMarked
+package io.github.dennisochulor.splashier_water.mixin;
+
+import org.jspecify.annotations.NullMarked;
